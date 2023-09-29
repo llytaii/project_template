4f21d04a3471033089b0c9ef1ebab67c282c8b3f
llytaii <llytaii@pm.me>
2023-09-29 20:11:32 +0200

    remove stopwatch

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,4 @@
-#include "stopwatch.hpp"
-
 #include <fmt/core.h>
 
 int main() {
-    Stopwatch sw;
-    fmt::print("\'Hello World!\'");
-    sw.stop();
-    fmt::print(" takes {} ms to print!", sw.duration<std::milli>());
 }
